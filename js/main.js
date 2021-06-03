ca7ed9a58e069aa27f9bf7e82eccad3b57ced274
Paulius B <Paulspremium@gmail.com>
2021-06-03 20:51:00 +0300

    events

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
-const logoDOM = document.querySelector('.logo');
-const hamburgerDOM = document.querySelector('.hamburger')
-const navDOM = document.querySelector('nav');
+let rezultatas = 0;
+const newAntraste = 'Zadimas procese!'// arba tiesiog priskirti teksta pakeitimui be kintamojo priskyrimo
 
+const minus = document.querySelector('.minus');
+const plius = document.querySelector('.plus');
+const result = document.querySelector('.number');
+const reset = document.querySelector('.reset')
+const antraste = document.querySelector('h1');
+const gryzta = antraste.innerText;
 
-console.log(navDOM);
-
-hamburgerDOM.addEventListener('click', () => {
-    navDOM.classList.toggle('visible');
-    logoDOM.classList.toggle('big');
+minus.addEventListener('click', () => {
+    result.innerText = --rezultatas;
+    antraste.innerText = newAntraste;
 })
+
+plius.addEventListener('click', () => {
+    result.innerText = ++rezultatas;
+    antraste.innerText = newAntraste;
+})
+
+reset.addEventListener('click', () => {
+    rezultatas = 0;
+    result.innerText = rezultatas;
+    antraste.innerText = gryzta;
+})
+
