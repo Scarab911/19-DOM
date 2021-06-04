59a75cfba3531333fae999e6e6294cec47d61212
Paulius B <Paulspremium@gmail.com>
2021-06-04 20:07:32 +0300

    manipaltion of html

--- js/main.js
+++ js/main.js
@@ -6,21 +6,33 @@ const plius = document.querySelector('.plus');
 const result = document.querySelector('.number');
 const reset = document.querySelector('.reset')
 const antraste = document.querySelector('h1');
+const ulDOM = document.querySelector('ul');
 const gryzta = antraste.innerText;
+// const pradinisUlHTML = ulDOM.innerHTML;   //nereikalingas
+
+
+
+ulDOM.innerHTML = `<li>Tu pasileidai zaidima - sekmes!</li>`
 
 minus.addEventListener('click', () => {
     result.innerText = --rezultatas;
     antraste.innerText = newAntraste;
+    ulDOM.innerHTML += `<li>Paspaudei minusa ir dabartinis rezultatas yra ${rezultatas}</li>`
+
+
 })
 
 plius.addEventListener('click', () => {
     result.innerText = ++rezultatas;
     antraste.innerText = newAntraste;
+    ulDOM.innerHTML += `<li>Paspaudei pliusa ir dabartinis rezultatas yra ${rezultatas}</li>`
 })
 
 reset.addEventListener('click', () => {
     rezultatas = 0;
     result.innerText = rezultatas;
     antraste.innerText = gryzta;
+    ulDOM.innerHTML = `<li>Paspaudei reset ir dabartinis rezultatas yra ${rezultatas}</li>`
+
 })
 
