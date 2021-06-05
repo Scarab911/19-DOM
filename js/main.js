2177634fd635341d8f7a7ec34e312b55b5817f9d
Paulius B <Paulspremium@gmail.com>
2021-06-05 11:46:31 +0300

    css if/else

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 const buttonDOM = document.querySelector('button');
-const inputDOM = document.querySelector('input');
+const messageInputDOM = document.querySelector('input');
 const spanDOM = document.querySelector('span');
 const fonoSpalva = document.querySelector('#bg-color');
 const tekstoSpalva = document.querySelector('#text-color');
 
 
 
-inputDOM.style.color = 'red';
+messageInputDOM.style.color = 'red';
+
 
 buttonDOM.addEventListener('click', (event) => {
     //isjungiam delfaultinesa savybes, kad nepersikrautu
     event.preventDefault();
     //priskiriam elemntui turini
-    spanDOM.innerText = inputDOM.value;
+    spanDOM.innerText = messageInputDOM.value;
     //priskiriam elemtui spalvas
-    spanDOM.style.color = `${tekstoSpalva.value}`;
-    spanDOM.style.backgroundColor = `${fonoSpalva.value}`;
+    spanDOM.style.color = tekstoSpalva.value;
+    spanDOM.style.backgroundColor = fonoSpalva.value;
     //isvalom forma
-    inputDOM.value = '';
+    buttonDOM.style.backgroundColor = fonoSpalva.value;
+    messageInputDOM.value = '';
 
 })
