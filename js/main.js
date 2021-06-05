0a8fbad3f7308658c26526e517d5722172a08afe
Paulius B <Paulspremium@gmail.com>
2021-06-05 10:46:54 +0300

    document colors manipulations

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
-const submitDOM = document.querySelector("button")
-const inputDOM = document.querySelector("input")
-const spanDOM = document.querySelector("span")
-const outputDOM = document.querySelector('.output')
+const buttonDOM = document.querySelector('button');
+const inputDOM = document.querySelector('input');
+const spanDOM = document.querySelector('span');
+const fonospalva = document.querySelector('#bg-color');
+const textcolor = document.querySelector('#text-color');
 
-// outputDOM.innerHTML = `<span></span>`
 
-submitDOM.addEventListener('click', (event) => {
+
+inputDOM.style.color = 'red';
+
+buttonDOM.addEventListener('click', (event) => {
+    //isjungiam delfaultinesa savybes, kad nepersikrautu
     event.preventDefault();
-    // outputDOM.innerHTML = `<span></span>`
+    //priskiriam elemntui turini
     spanDOM.innerText = inputDOM.value;
+    //priskiriam elemtui spalvas
+    spanDOM.style.color = `${textcolor.value}`;
+    spanDOM.style.backgroundColor = `${fonospalva.value}`;
+    //isvalom forma
     inputDOM.value = '';
+
 })
