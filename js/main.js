cdc225457c91c1f03d3c04fe21feaa58cffb29e7
Paulius B <Paulspremium@gmail.com>
2021-06-05 10:12:42 +0300

    10 exersize

--- js/main.js
+++ js/main.js
@@ -1,38 +1,13 @@
-let rezultatas = 0;
-const newAntraste = 'Zadimas procese!'// arba tiesiog priskirti teksta pakeitimui be kintamojo priskyrimo
+const submitDOM = document.querySelector("button")
+const inputDOM = document.querySelector("input")
+const spanDOM = document.querySelector("span")
+const outputDOM = document.querySelector('.output')
 
-const minus = document.querySelector('.minus');
-const plius = document.querySelector('.plus');
-const result = document.querySelector('.number');
-const reset = document.querySelector('.reset')
-const antraste = document.querySelector('h1');
-const ulDOM = document.querySelector('ul');
-const gryzta = antraste.innerText;
-// const pradinisUlHTML = ulDOM.innerHTML;   //nereikalingas
+// outputDOM.innerHTML = `<span></span>`
 
-
-
-ulDOM.innerHTML = `<li>Tu pasileidai zaidima - sekmes!</li>`
-
-minus.addEventListener('click', () => {
-    result.innerText = --rezultatas;
-    antraste.innerText = newAntraste;
-    ulDOM.innerHTML += `<li>Paspaudei minusa ir dabartinis rezultatas yra ${rezultatas}</li>`
-
-
+submitDOM.addEventListener('click', (event) => {
+    event.preventDefault();
+    // outputDOM.innerHTML = `<span></span>`
+    spanDOM.innerText = inputDOM.value;
+    inputDOM.value = '';
 })
-
-plius.addEventListener('click', () => {
-    result.innerText = ++rezultatas;
-    antraste.innerText = newAntraste;
-    ulDOM.innerHTML += `<li>Paspaudei pliusa ir dabartinis rezultatas yra ${rezultatas}</li>`
-})
-
-reset.addEventListener('click', () => {
-    rezultatas = 0;
-    result.innerText = rezultatas;
-    antraste.innerText = gryzta;
-    ulDOM.innerHTML = `<li>Paspaudei reset ir dabartinis rezultatas yra ${rezultatas}</li>`
-
-})
-
