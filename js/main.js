162c146fcaf76b5084e827c79e94cbd5733df44e
Paulius B <Paulspremium@gmail.com>
2021-06-07 18:29:49 +0300

    updated js

--- js/main.js
+++ js/main.js
@@ -6,23 +6,18 @@ const desertas = document.querySelector('#desertas');
 const uzsakymas = document.querySelector('.order')
 const gerimas = document.querySelectorAll('input[name="drinks"]')
 
-mygtukas.addEventListener('click', (event) => {
-    event.preventDefault();
+mygtukas.addEventListener('click', (e) => { //e=event, trumpiausias naudojamas parametras 
+    e.preventDefault();
 
     function menuCheked(item) {
-        if (item.checked) {
-            return 'nori'
-        }
-        return 'nenori'
+        return item.checked ? 'nori' : 'nenori';
     }
     function drinkCheked(item) {
         for (let i = 0; i < item.length; i++) {
             let gerimoPavadinimas = ''
             if (item[i].checked === true) {
                 return gerimoPavadinimas = `${item[i].value}`
-
             }
-
         }
     }
     uzsakymas.innerText = `Uzsakovas vardu ${klientas.value}, ${menuCheked(sriuba)} sriubos, ${menuCheked(patiekalas)} pagrindinio patiekalo, ${menuCheked(desertas)} deserto ir ${drinkCheked(gerimas)} yra pasirinktas gerimas.`;
