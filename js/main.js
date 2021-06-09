6603610c9977503844481db2a481e5f81abcdd3d
Paulius B <Paulspremium@gmail.com>
2021-06-09 18:19:57 +0300

    import of icons excersise

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-function generuotiSocials() {
+function generuotiSocials(selector, array) {
+    const selectorDOM = document.querySelector(selector);
 
-    return
+    if (!Array.isArray(array) ||
+        array.length === 0) {
+        console.error('ERROR: sarasas turi buti ne tuscias!');
+        return false;
+    }
+    let HTML = '';
+
+    for (let element of array) {
+        HTML += `<i class="fa fa-${element}"></i>`
+        console.log(HTML);
+    }
+    selectorDOM.innerHTML = HTML;
 }
 
-const incons = ['facebook', 'twitter', 'linkedin'];
-
+const icons = ['facebook', 'twitter', 'linkedin'];
+const icons1 = [];
 generuotiSocials('.socials', icons);
