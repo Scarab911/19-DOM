9a555e4eb5d2806168e050dce0b5ed3e1355b206
Paulius B <Paulspremium@gmail.com>
2021-06-09 20:44:06 +0300

    Validations

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
 function generuotiSocials(selector, array) {
-    const selectorDOM = document.querySelector(selector);
 
     if (!Array.isArray(array) ||
         array.length === 0) {
         console.error('ERROR: sarasas turi buti ne tuscias!');
         return false;
     }
+
+    const selectorDOM = document.querySelector(selector);
+    if (!selectorDOM) {
+        console.error('ERROR: nepavyko rasti selectoriaus');
+        return false;
+    }
+
+    if (typeof selector !== 'string' ||
+        selector === '') {
+        console.error('ERROR: selectorius negali buti tuscias string!');
+        return false;
+    }
+
     let HTML = '';
 
     for (let element of array) {
         HTML += `<i class="fa fa-${element}"></i>`
     }
+    if (HTML === '') {
+        console.error('ERROR: kodo dalis negali buti tuscia ir turi buti string');
+        return false
+    }
+
     selectorDOM.innerHTML = HTML;
 }
 
