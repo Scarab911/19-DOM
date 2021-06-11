913ee2da7fc8ec8feafc45b288bc78b3d9eb62dc
Paulius B <Paulspremium@gmail.com>
2021-06-11 16:41:40 +0300

    17-forma-button-final

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const gyvunai = ['zuikis', 'barsukas', 'lape', 'vilkas', 'sernas', 'stirna', 'vovere'];
+
+function addAndPrint(selector, data) {
+    //Validations
+
+    //Logic
+    const selectorDOM = document.querySelector(selector);
+    const labelDOM = document.querySelector('label');
+    const button = document.querySelector('button');
+
+    let HTML = '';
+
+    for (const item of data) {
+        HTML += `<option value="${item}">${item}</option>`
+    }
+
+    labelDOM.insertAdjacentHTML('afterend', `<select>${HTML}</select>`);
+
+    const selectDOM = document.querySelector('select');
+    selectDOM.classList.add('pomidoras'); //uzdedam klase kad pirma raide butu didzioji
+    const spanDOM = document.querySelector('.option');
+    spanDOM.classList.add('pomidoras');
+
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        spanDOM.innerText = selectDOM.value;
+    });
+
+    // post Logic Validations
+}
+
+console.log(addAndPrint('form', gyvunai));
