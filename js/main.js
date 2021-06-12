085d083779760278b38c0f5d923ec41bbcd84e97
Paulius B <Paulspremium@gmail.com>
2021-06-12 14:05:09 +0300

    lentos row ir cell generavimas

--- js/main.js
+++ js/main.js
@@ -8,11 +8,24 @@ const board = document.querySelector('.board')
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
-    const n = input.value;
+    const n = parseInt(input.value);//paverciam teksta i skaiciu!
     option.innerText = `${n}x${n}`;
+    generateChessBoard(n, board);
 })
 
-function generateChessBoard() {
 
+function generateChessBoard(lentosDydis, DOM) {
+
+    let rowHTML = '';
+    let celleHTML = '';
+    const dydis = 100 / lentosDydis;
+
+    for (let j = 1; j <= lentosDydis; j++) {
+        celleHTML += `<div class="cell" style="width:${dydis}%;"></div>`;
+    }
+
+    for (let i = 1; i <= lentosDydis; i++) {
+        rowHTML += `<div class="row" style="height:${dydis}%;">${celleHTML}</div>`
+    }
+    DOM.innerHTML = rowHTML;
 }
-generateChessBoard();
