f843617214897fe9e8f8e355ef9a392163d2f53a
Paulius B <Paulspremium@gmail.com>
2021-06-14 18:01:09 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-
-//pirma dalis
 const button = document.querySelector('button');
-const input = document.querySelector('#board')
+const input = document.getElementById('board')
 const option = document.querySelector('.option')
-//antra dalis
 const board = document.querySelector('.board')
 
 button.addEventListener('click', (e) => {
@@ -13,7 +10,7 @@ button.addEventListener('click', (e) => {
     generateChessBoard(n, board);
 })
 
-
+//Lentos generavimo funkcija:
 function generateChessBoard(lentosDydis, DOM) {
 
     let rowHTML = '';
@@ -28,4 +25,15 @@ function generateChessBoard(lentosDydis, DOM) {
         rowHTML += `<div class="row" style="height:${dydis}%;">${celleHTML}</div>`
     }
     DOM.innerHTML = rowHTML;
+
+    const allCells = DOM.querySelectorAll('.cell')
+    const langeliai = Array.from(allCells)
+    console.log(langeliai)
+
+    for (let k = 0; k < langeliai.length; k++) {
+        const langelis = langeliai[k];
+        if (k % 2 === 0) {
+            langelis.classList.add('black')
+        }
+    }
 }
